5b5f36eac2ee7547d8f848d701b5a6d1b52abee7
mohammedaboohamid <mohammedaboohamidkp@gmail.com>
2024-10-10 14:44:43 +0530

    environmental veriable

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import app from "./src/app.js";
 import colors from "colors"
+import config from "./src/config/config.js";
 
-const PORT =4000
+const PORT =config.PORT
 
 app.listen(PORT,()=>{
     console.log(`App is running at PORT:${PORT}` .bgBlack.rainbow);
